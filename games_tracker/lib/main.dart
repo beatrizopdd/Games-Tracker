9fd21e7a63042daccac988b0d54e75c81b0359a9
fredericojpoli <143126574+fredericojpoli@users.noreply.github.com>
2024-07-04 17:11:10 -0300

    Pushado!

---
+++ games_tracker/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+//import 'pages/results.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Games Tracker", debugShowCheckedModeBanner: false, home: Home()));
+}
